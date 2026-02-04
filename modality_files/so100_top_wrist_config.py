e595a0fb7172ccfa79deaab4f51e74d86e272d8b
alex-luci <alexandru.luci@rebeldot.com>
2026-02-04 19:49:06 +0200

    release: first test sa vedem daca crapa la 11

---
+++ modality_files/so100_top_wrist_config.py
@@ -0,0 +1,48 @@
+from gr00t.configs.data.embodiment_configs import register_modality_config
+from gr00t.data.embodiment_tags import EmbodimentTag
+from gr00t.data.types import (
+    ActionConfig,
+    ActionFormat,
+    ActionRepresentation,
+    ActionType,
+    ModalityConfig,
+)
+so100_config = {
+    "video": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["top", "wrist"],  # Changed from ["front", "wrist"] to match your dataset
+    ),
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "single_arm",
+            "gripper",
+        ],
+    ),
+    "action": ModalityConfig(
+        delta_indices=[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        ],
+        modality_keys=[
+            "single_arm",
+            "gripper",
+        ],
+        action_configs=[
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+        ],
+    ),
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.task_description"],  # Matches your modality.json
+    ),
+}
+register_modality_config(so100_config, embodiment_tag=EmbodimentTag.NEW_EMBODIMENT)
